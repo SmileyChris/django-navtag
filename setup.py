5601087312b7475f420d228e5f6041d4de173948
Chris Beaven <smileychris@gmail.com>
2013-07-18 13:26:47 +1200

    Make 'python setup.py test' work

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
-import setuptools
+import sys
 
-setuptools.setup(
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class Tox(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
+setup(
     name='django-navtag',
     version='1.0',
     description=("A simple Django navigation template tag"),
     author='Chris Beaven',
     author_email='smileychris@gmail.com',
     url='http://github.com/SmileyChris/django-navtag',
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     include_package_data=True,
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
