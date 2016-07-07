029b6adab9cb2c77162b675c2b47d6f37ace9acd
Michael Fladischer <FladischerMichael@fladi.at>
2016-07-07 20:31:12 +0200

    Add TEMPLATES setting as required by Django 1.8+.
    This prevents tests from emitting a warning for Django 1.8 to 1.10. The tests
    would also fail for the to-be-released Django 1.11 without this setting.

--- django_navtag/tests/settings.py
+++ django_navtag/tests/settings.py
@@ -15,3 +15,19 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+            ],
+        },
+    },
+]
