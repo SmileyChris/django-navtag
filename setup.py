4d58305f255cd7379b70e91b669c7b3ae17ce7e8
Chris Beaven <smileychris@gmail.com>
2018-02-07 15:53:12 +1300

    Update tox, use zest.releaser for simple releasing

--- setup.py
+++ setup.py
@@ -1,44 +1,4 @@
-import sys
+#!/usr/bin/env python
+from setuptools import setup
 
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-
-
-class Tox(TestCommand):
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import tox
-        errno = tox.cmdline(self.test_args)
-        sys.exit(errno)
-
-
-setup(
-    name='django-navtag',
-    version='2.1.2',
-    description=("A simple Django navigation template tag"),
-    author='Chris Beaven',
-    author_email='smileychris@gmail.com',
-    url='http://github.com/SmileyChris/django-navtag',
-    packages=find_packages(),
-    include_package_data=True,
-    tests_require=['tox'],
-    cmdclass={'test': Tox},
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-    ]
-)
+setup()
